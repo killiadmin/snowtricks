f02bca16999c0d53108f5d529d54ae81019c9efc
killiadmin <k.filatre@gmail.com>
2023-10-27 10:11:56 +0200

    correction name entitie 'video_figure'

---
+++ migrations/Version20231027062607.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20231027062607 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE figure CHANGE video_picture video_figure VARCHAR(255) NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE figure CHANGE video_figure video_picture VARCHAR(255) NOT NULL');
+    }
+}
