89c5a98912f4deca2c1e4b4131501c88d605458a
killiadmin <k.filatre@gmail.com>
2024-02-02 23:04:35 +0100

    clean code and updated package

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -119,19 +119,4 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     {
         return $this->findOneBySomeField('newToken', $token);
     }
-
-//    /**
-//     * @return User[] Returns an array of User objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('u')
-//            ->andWhere('u.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('u.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
 }
